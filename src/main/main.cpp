028ecba7f18a98a1a9350cc84385c9addadaf3ca
Marcelo Pinto <mpinto70@gmail.com>
2019-07-02 00:27:23 -0400

    add jupiter

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -52,24 +52,32 @@ void simulate(space::System& s, size_t years, size_t print_interval) {
 }
 
 int main() {
-    constexpr double earth_mass = 5.97237e24;           // mass in kg
-    constexpr double distance_earth_to_sun = 147.095e9; // perihelion
-    constexpr double earth_orbital_speed = 30.3e3;      // in m/s
     constexpr double sun_mass = 1.9885e30;
 
     // sun at the center of the reference plan
     constexpr geometry::vec3d sun_position{ 0, 0, 0 };
-    constexpr geometry::vec3d sun_velocity{ 0, 0, 0 };
+    constexpr geometry::vec3d sun_velocity{ 0, 12.4, 0 };
     const space::Particle sun_particle(sun_mass, sun_position, sun_velocity);
 
-    // earth positioned at the x(+) axis with speed in the y direction
+    // earth positioned at the x(+) axis with speed in the y(+) direction
+    constexpr double earth_mass = 5.97237e24;           // mass in kg
+    constexpr double distance_earth_to_sun = 147.095e9; // perihelion
+    constexpr double earth_orbital_speed = 30.3e3;      // in m/s
     constexpr geometry::vec3d earth_position{ distance_earth_to_sun, 0, 0 };
     constexpr geometry::vec3d earth_velocity{ 0, earth_orbital_speed, 0 };
     const space::Particle earth_particle(earth_mass, earth_position, earth_velocity);
 
-    space::System s({ sun_particle, earth_particle });
+    // jupiter positioned at the x(-) axis with speed in the y(-) direction
+    constexpr double jupiter_mass = 1.8982E27;           // mass in kg
+    constexpr double distance_jupiter_to_sun = 778.57e9; // mean
+    constexpr double jupiter_orbital_speed = 13.07e3;    // in m/s
+    constexpr geometry::vec3d jupiter_position{ -distance_jupiter_to_sun, 0, 0 };
+    constexpr geometry::vec3d jupiter_velocity{ 0, -jupiter_orbital_speed, 0 };
+    const space::Particle jupiter_particle(jupiter_mass, jupiter_position, jupiter_velocity);
 
-    constexpr size_t years_transient = 1500;
+    space::System s({ sun_particle, earth_particle, jupiter_particle });
+
+    constexpr size_t years_transient = 100;
     constexpr size_t years_monitor = 200;
     constexpr size_t print_interval = 1;
     std::cerr << "skipping transient period of " << years_transient << " years\n";
