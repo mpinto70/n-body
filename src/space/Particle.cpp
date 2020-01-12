eb25a1f4b3124e2bf58368db291f1a002f61a54e
Marcelo Pinto <mpinto70@gmail.com>
2020-01-11 19:21:05 -0500

    format

--- src/space/Particle.cpp
+++ src/space/Particle.cpp
@@ -7,7 +7,10 @@ static_assert(std::is_trivially_copyable<geometry::vec3d>::value);
 
 //cppcheck-suppress passedByValue
 Particle::Particle(std::string name, double mass, geometry::vec3d position, geometry::vec3d velocity)
-      : name_(std::move(name)), mass_(mass), position_(position), velocity_(velocity) {
+      : name_(std::move(name)),
+        mass_(mass),
+        position_(position),
+        velocity_(velocity) {
 }
 
 void Particle::move(double delta_t, const geometry::vec3d& acceleration) {
